38825ce6b76a9e909379cbbe925efcdc54d74fd1
xiaop1ng <784516419@qq.com>
2019-02-15 13:36:57 +0800

    config modify

--- config-client/src/main/java/com/xiaoping/ConfigClientApp.java
+++ config-client/src/main/java/com/xiaoping/ConfigClientApp.java
@@ -2,10 +2,13 @@ package com.xiaoping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigClientApp {
 
     public static void main(String[] args) {
